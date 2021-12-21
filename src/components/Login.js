8a7d060c185175bb88f1e519383e5a73835e58a5
SpencerTower <spencerjtower1@gmail.com>
2021-12-20 17:19:11 -0800

    adds Login.js and LoginBtn.js outline, adds inital Heaer.js file

---
+++ src/components/Login.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import LoginBtn from './LoginForm';
+
+
+class Login extends Component {
+  render() {
+    return (
+      <div>
+        <LoginBtn />
+      </div>
+    );
+  }
+}
+
+export default Login;
---
+++ src/components/LoginBtn.js
@@ -0,0 +1,16 @@
+// import Button from 'react-bootstrap/Button';
+
+
+
+
+class LoginBtn extends Component {
+  render() {
+    return (
+      <div>
+        <Button onClick={}>Log In</Button>
+      </div>
+    );
+  }
+}
+
+export default LoginBtn;
