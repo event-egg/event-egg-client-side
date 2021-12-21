de3c02048f041a7cf79cb1d11921598ec2955d58
SpencerTower <spencerjtower1@gmail.com>
2021-12-20 18:19:24 -0800

    adds conditionals to render Login, Header, Footer, Welcome, Router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
-import './App.css';
-
 import React, { Component } from 'react';
+import './App.css';
+import { Login, Header, Footer, Welcome } from './components';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 class App extends Component {
+
+
+  tempVars = {
+    isAuthenticated: true,
+    userData: {}
+  }
+
+
+
   render() {
     return (
-      <div>
-        <h1>Hi</h1>
-      </div>
+      <>
+        {!this.tempVars.isAuthenticated ?
+          <Login /> :
+          <>
+            <Header />
+            {/* if user data doesnt exist,  render welcome page, else router */}
+            {Object.keys(this.tempVar.userData).length === 0 ?
+              <Welcome /> :
+              <Router>
+            }
+
+
+                <Footer />
+              </>
+            }
+          </>
     );
   }
 }
 
-export default App;
+        export default App;
