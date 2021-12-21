76cdd692ddce09bdd4c715bf30f23509f044856f
SpencerTower <spencerjtower1@gmail.com>
2021-12-20 18:28:37 -0800

    adds Routes for myEvents, profile, about

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Login, Header, Footer, Welcome } from './components';
+import { Login, Header, Footer, Welcome, Dashboard, MyEvents, Profile, About } from './components';
 import {
   BrowserRouter as Router,
   Switch,
   Route
 } from "react-router-dom";
+import MyEvents from './components/MyEvents';
 
 class App extends Component {
 
@@ -28,15 +29,28 @@ class App extends Component {
             {Object.keys(this.tempVar.userData).length === 0 ?
               <Welcome /> :
               <Router>
+                <Switch>
+                  <Route exact path="/">
+                    <Dashboard />
+                  </Route>
+                  <Route exact path="/myEvents">
+                    <MyEvents />
+                  </Route>
+                  <Route exact path="/profile">
+                    <Profile />
+                  </Route>
+                  <Route exact path="/about">
+                    <About />
+                  </Route>
+                </Switch>
+              </ Router>
             }
-
-
-                <Footer />
-              </>
-            }
+            <Footer />
           </>
+        }
+      </>
     );
   }
 }
 
-        export default App;
+export default App;
