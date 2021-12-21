8650e05e74b5d4d1c129a59295c6648c0319ed87
SpencerTower <spencerjtower1@gmail.com>
2021-12-20 22:50:57 -0800

    adds EventCard to MyEvents

--- src/components/MyEvents.js
+++ src/components/MyEvents.js
@@ -1,5 +1,6 @@
 
 import React, { Component } from 'react';
+import EventCard from './EventCard';
 
 // renders user's saved events
 
@@ -9,6 +10,7 @@ class MyEvents extends Component {
     return (
       <div>
         <h1>MyEvents</h1>
+        <EventCard />
       </div>
     );
   }
