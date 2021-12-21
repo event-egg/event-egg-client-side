30bee9665c34beab2233e837ba261a2cd7236a00
SpencerTower <spencerjtower1@gmail.com>
2021-12-20 17:28:08 -0800

    adds LoginForm.js outline

---
+++ src/components/LoginForm.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button'
+
+// Basic outline of form
+
+class LoginForm extends Component {
+  render() {
+    return (
+      <div>
+        {/* <Form onSubmit={this.handleSubmit}>
+          <Form.Group className="mb-3" controlId="formBasicCity" >
+            <Form.Label>City</Form.Label>
+            <Form.Control type="text" placeholder="city" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+          <Button variant="primary" type="">
+            Skip
+          </Button>
+        </Form> */}
+      </div>
+    );
+  }
+}
+
+export default LoginForm;
