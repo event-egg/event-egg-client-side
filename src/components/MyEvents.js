b372b2bf5f2bd7d39255d90ba50d7d4dfa8cc4b3
SpencerTower <spencerjtower1@gmail.com>
2021-12-20 17:37:05 -0800

    adds LogoutBtn.js, Welcome.js, and MyEvents.js outlines

---
+++ src/components/MyEvents.js
@@ -0,0 +1,17 @@
+
+import React, { Component } from 'react';
+
+// renders user's saved events
+
+
+class MyEvents extends Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default MyEvents;
