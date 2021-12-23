fd5f3f1fed1ac7ad9a85789b6f0dacfd8f613684
SpencerTower <spencerjtower1@gmail.com>
2021-12-23 14:49:48 -0800

    adds AboutUsData.js

---
+++ src/AboutUsData.js
@@ -0,0 +1,31 @@
+const aboutUsArray = [
+  {
+    name: "Kellen Linse",
+    image: "",
+    about: "",
+    githubLink: "",
+    linkedinLink: ""
+  },
+  {
+    name: "Spencer Tower",
+    image: "",
+    about: "",
+    githubLink: "",
+    linkedinLink: ""
+  },
+  {
+    name: "Andrew Enyeart",
+    image: "",
+    about: "",
+    githubLink: "",
+    linkedinLink: ""
+  },
+  {
+    name: "Daniel Jackson",
+    image: "",
+    about: "",
+    githubLink: "",
+    linkedinLink: ""
+  }
+]
+export default aboutUsArray;
