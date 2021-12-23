9e75516cb0def3076ffbe1bf8ffd0119679da5d7
Andrew Enyeart <reallyhardtoforget@gmail.com>
2021-12-23 11:40:05 -0800

    Updates format of rendered events

--- src/components/MyEvents.js
+++ src/components/MyEvents.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import EventCard from './EventCard';
+import Row from 'react-bootstrap/Row';
 
 // renders user's saved events
 // event cards will render 'remove event' btn instead of 'save event'
@@ -9,10 +10,11 @@ class MyEvents extends Component {
     return (
       <div>
         <h1>MyEvents</h1>
-        {this.props.user.savedEvents.length > 0 && this.props.user.savedEvents.map((event, idx) => {
-          console.log(event)
-          return <EventCard type="myEvent" event={event} key={event.id + idx} />
-        })}
+        {this.props.user.savedEvents.length > 0 && 
+        <Row sm={1} md={2} lg={5}>
+        {this.props.user.savedEvents.map((event, idx) =>
+          <EventCard type="myEvent" event={event} key={event.id + idx} />
+        )}</Row>}
       </div>
     );
   }
