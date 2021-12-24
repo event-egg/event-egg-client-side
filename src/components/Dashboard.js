3fc83e03800d46871fc1cc8d8c179501d18c06dd
Kellen-Linse <kblinse06@gmail.com>
2021-12-23 19:35:49 -0800

    Adds basic search functionality

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import EventCard from './EventCard';
 import Row from 'react-bootstrap/Row'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 import axios from 'axios';
 
 
@@ -8,7 +10,7 @@ import axios from 'axios';
 class Dashboard extends Component {
 
   componentDidMount = () => {
-    this.getEvents();
+    this.getEvents('seattle');
   }
 
 
@@ -19,9 +21,15 @@ class Dashboard extends Component {
     }
   }
 
+  handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(e.target.exampleForm.value)
+    this.getEvents(e.target.exampleForm.value)
+  }
+
 
   getEvents = async (keyword) => {
-    console.log("Get Events");
+    console.log("Get Events:", keyword);
     const res = await this.props.auth0.getIdTokenClaims();
     // put token in variable
     const jwt = res.__raw;
@@ -29,7 +37,7 @@ class Dashboard extends Component {
       method: 'get',
       // change back to process.env
       baseURL: 'http://localhost:3001',
-      url: `/events?keyword=basketball`,
+      url: `/events?keyword=${keyword}`,
       headers: {
         "Authorization": `Bearer ${jwt}`
       }
@@ -44,6 +52,13 @@ class Dashboard extends Component {
     return (
       <div>
         <h1>{this.props.user.defaultCity}</h1> 
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Group className="mb-3" controlId="exampleForm">
+            <Form.Label>Example textarea</Form.Label>
+            <Form.Control as="textarea" rows={3} />
+          </Form.Group>
+          <Button type='submit' name='submit' >Submit</Button>
+        </Form>
         { this.state.events.length > 0   &&
         <Row sm={1} md={2} lg={5}>
             {this.state.events.length > 0 && this.state.events.map(event => <EventCard type="newEvent" event={event} key={event.id} user={this.props.user} saveEvent={this.props.saveEvent} />)}
