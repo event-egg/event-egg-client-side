1590c96db388a7a6fb88da18482bb3a365a075aa
SpencerTower <spencerjtower1@gmail.com>
2021-12-26 14:07:02 -0800

    displays message on MyEvents page if user has no saved events

--- src/components/MyEvents.js
+++ src/components/MyEvents.js
@@ -7,18 +7,18 @@ import Container from 'react-bootstrap/Container';
 // event cards will render 'remove event' btn instead of 'save event'
 
 class MyEvents extends Component {
-  
+
 
 
   render() {
     return (
       <div>
         <h1>MyEvents</h1>
-        {this.props.user.savedEvents.length > 0 && 
-        <Row sm={1} md={2} lg={5}>
-        {this.props.user.savedEvents.map((event, idx) =>
-          <EventCard type="myEvent" event={event} key={event.id} deleteEvent={this.props.deleteEvent} user={this.props.user}/>)}
-        </Row>}
+        {this.props.user.savedEvents.length > 0 ?
+          <Row sm={1} md={2} lg={5}>
+            {this.props.user.savedEvents.map((event, idx) =>
+              <EventCard type="myEvent" event={event} key={event.id} deleteEvent={this.props.deleteEvent} user={this.props.user} />)}
+          </Row> : <p>Looks like you don't have any saved events here yet! Why not go to your Dashboard and add some events that look promising?</p>}
       </div>
     );
   }
