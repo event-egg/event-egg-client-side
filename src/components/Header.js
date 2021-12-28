2db35d497c219a5031da77fa88de64da60a40efb
Kellen-Linse <kblinse06@gmail.com>
2021-12-27 20:16:41 -0800

    Add styling to login, add logo to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import Navbar from 'react-bootstrap/Navbar';
 import NavItem from 'react-bootstrap/NavItem';
 import { Link } from 'react-router-dom';
 import LogoutBtn from './LogoutBtn';
+import { Container } from 'react-bootstrap';
 
 
 
@@ -13,12 +14,24 @@ class Header extends Component {
     return (
       <div>
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" sticky="top">
-          <Navbar.Brand>EventEgg</Navbar.Brand>
+        <Container>
+          <Navbar.Brand href="/">
+            <img
+              alt=""
+              src='./img/white-egg8bit.png'
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />
+          Event Egg
+          </Navbar.Brand>
+        
           <NavItem><Link to="/" className="nav-link">Dashboard</Link></NavItem>
           <NavItem><Link to="/myEvents" className="nav-link">My Events</Link></NavItem>
           <NavItem><Link to="/profile" className="nav-link">Profile</Link></NavItem>
           <NavItem><Link to="/about" className="nav-link">About</Link></NavItem>
           <NavItem><LogoutBtn className="nav-link">Log Out</LogoutBtn></NavItem>
+          </Container>
         </Navbar>
       </div >
     );
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,21 +1,32 @@
 import React, { Component } from 'react';
 import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
 import { useAuth0 } from '@auth0/auth0-react';
+import { Col, Image } from 'react-bootstrap';
 
 function LoginButton(props) {
   const { loginWithRedirect } = useAuth0();
-  return <Button variant="outline-info" onClick={() => {
+  return <Button className="mx-auto px-5 py-4 text-nowrap" style={{minWidth: '30%', textAlign: 'center', textDecoration: 'underline', fontSize:'2rem', background: 'white', boxShadow: '3px 3px 2px 2px #0000003f'}} variant="outline-info" onClick={() => {
     loginWithRedirect(); 
-  }}>Log In</ Button>
+  }}><strong>Log In</strong></ Button>
 };
 
 class Login extends Component {
   render() {
     return (
-      <div>
-        <h1>Event Egg</h1>
-          <LoginButton />
-      </div>
+      <Container style={{height: '100vh', background: 'linear-gradient(90deg, rgba(160,155,232,1) 9%, rgba(124,207,143,1) 52%, rgba(0,212,255,1) 100%)' }} fluid>
+        <Col className='p-5 d-flex justify-content-between flex-column pt-5 mb-0' style={{height: '100%'}}>
+          <Container  className="d-flex justify-content-around mt-5 pb-5" fluid>
+          <h1 style={{minWidth: '30%', textDecoration: 'underline'}} className='text-center text-nowrap display-1' fluid><strong>Event Egg</strong></h1>
+          </Container>
+          <Container className="d-flex justify-content-around m-0 p-0" fluid>
+            <Image style={{minWidth: '300px'}} src='./img/white-egg8bit.png' fluid/>
+          </Container>
+          <Container className="d-flex justify-content-around mb-5 pt-5 pb-5" fluid>
+            <LoginButton />
+          </Container>
+        </Col>
+      </Container>
     );
   }
 }
